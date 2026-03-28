bf5ffb6b1d6b9e63cd7650fe859e5690adee8c1b
Joshua Farmer <rhubarbjosh@outlook.com>
2026-03-28 22:58:09 +0000

    fixed

--- tests/window.s
+++ tests/window.s
@@ -8,16 +8,14 @@ _foo:
         AND     $0C, $00, $00
         ADD     $00, $00, $0D
         ;       set new sp
-        STW     $0C, $00, $00
+        STW     $00, $0C, $00
         ;       set new pc = body
         LDI     $0D, body
-        STW     $0C, $0D, $3C
+        STW     $0D, $0C, $3C
         ;       set parent window
         CTX     $0D
-        STW     $0C, $0D, $34
+        STW     $0D, $0C, $38
         ;       switch
         CTS     $0C
-        HLT
         LNK     $00,$00
-body:   HLT
-        CTS     $0E
+body:   CTS     $0E
